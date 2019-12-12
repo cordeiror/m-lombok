6576cf335ac2b01d575d1a2700d5a629862a18ec
renato <renato.cordeiro@gmail.com>
2019-12-12 15:36:02 -0500

    first commit

---
+++ src/main/java/cords/model/User.java
@@ -0,0 +1,20 @@
+package cords.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+
+}
